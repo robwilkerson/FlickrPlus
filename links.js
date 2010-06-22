28774f07471e361043d062368fd458ed2076a94f
Rob Wilkerson <rob@robwilkerson.org>
2010-06-22 09:41:33 -0400

    Adding links to view each image size right on the image page rather than having to click through.

--- links.js
+++ links.js
@@ -1,18 +1,30 @@
 $(document).ready( function() {
-	/**
-	var img_uri = $('.photoImgDiv img').attr( 'src' );
-	console.log( 'source: ' + img_uri );
-	var img_base = img_uri.match( /[^.]/gi );
-	console.log( img_base );
-	*/
+	var base_view_uri = $('link[rel="canonical"]').attr( 'href' );
+	var sizes = {
+		'Original':  base_view_uri + 'sizes/o',
+		'Large':     base_view_uri + 'sizes/l',
+		'Medium':    base_view_uri + 'sizes/m',
+		'Small':     base_view_uri + 'sizes/s',
+		'Thumbnail': base_view_uri + 'sizes/t',
+		'Square':    base_view_uri + 'sizes/sq'
+	}
+	var size_markup = function() {
+		var html = new Array();
+		
+		for( var size in sizes ) {
+			html.push( '<a href="' + sizes[size] + '">' + size + '</a>' );
+		}
+		
+		return html.join( ', ' );
+	}
 	
 	$('.PeopleTagList').before( '<div class="FlickrPlus"></div>' );
 	$('.FlickrPlus').html( '<h4>Flickr<strong>Plus</strong></h4>');
 	$('.FlickrPlus').append( function() {
 		return '<ul>' +
 	         '<li class="Stats"><a href="http://fiveprime.org/blackmagic" rel="nofollow">View on Black</a></li>' +
-					 // '<li class="Stats">Sizes: <a href=""></a>, <a href=""></a></li>' +
-					 '<li class="Stats">Short URL: <a href="' + $('link[href^="http://flic.kr/p/"]').attr( 'href' ) + '" rel="nofollow">' + $('link[href^="http://flic.kr/p/"]').attr( 'href' ) + '</a></li>' +
+					 '<li class="Stats">View: ' + size_markup() + '</li>' +
+					 '<li class="Stats">Short URL: <a href="' + $('link[rev="canonical"]').attr( 'href' ) + '" rel="nofollow">' + $('link[href^="http://flic.kr/p/"]').attr( 'href' ) + '</a></li>' +
 					 '</ul>';
 	});
 });
