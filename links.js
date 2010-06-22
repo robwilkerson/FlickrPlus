13b9de39190f12e0af39928c7210ac58765b5b49
Rob Wilkerson <rob@robwilkerson.org>
2010-06-21 23:07:40 -0400

    Updating the display to reflect the FlickrPlus name rather than the temporary name.

--- links.js
+++ links.js
@@ -6,9 +6,9 @@ $(document).ready( function() {
 	console.log( img_base );
 	*/
 	
-	$('.PeopleTagList').before( '<div class="FlickrTools"></div>' );
-	$('.FlickrTools').html( '<h4>FlickrTools</h4>');
-	$('.FlickrTools').append( function() {
+	$('.PeopleTagList').before( '<div class="FlickrPlus"></div>' );
+	$('.FlickrPlus').html( '<h4>Flickr<strong>Plus</strong></h4>');
+	$('.FlickrPlus').append( function() {
 		return '<ul>' +
 	         '<li class="Stats"><a href="http://fiveprime.org/blackmagic" rel="nofollow">View on Black</a></li>' +
 					 // '<li class="Stats">Sizes: <a href=""></a>, <a href=""></a></li>' +
