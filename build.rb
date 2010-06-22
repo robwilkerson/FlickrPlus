3431940dbdcc408e54fd76fd57ae22335b58700e
Rob Wilkerson <rob@robwilkerson.org>
2010-06-21 23:08:23 -0400

    Adding an update manifest and build file.

---
+++ build.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/ruby 
+
+require 'logger'
+require 'FileUtils'
+
+build_dir = '../builds/' + File.basename( FileUtils.pwd )
+
+puts 'Generating a build in ' + build_dir
+puts '  -> Removing existing directory (' + build_dir + ')'
+
+FileUtils.rm_r build_dir
+
+puts '  -> Recreating the directory (' + build_dir + ')'
+
+FileUtils.mkdir_p build_dir
+
+puts '  -> Archiving the latest copy of the master branch'
+
+system 'git archive master | tar x -C ../builds/' + File.basename( FileUtils.pwd )
+
+puts 'Switching to the build directory'
+
+Dir.chdir build_dir
+
+puts '  -> Deleting files not required for execution of the extension'
+
+FileUtils.rm Dir.glob( 'README.*' )
+FileUtils.rm Dir.glob( '*-update.plist' )
+
+puts 'Done.'
+puts ''
+puts 'Don\'t forget to: '
+puts '  1. Open the build directory in extension builder and compile the extension.'
+puts '  2. SCP the compiled extension and the updater as shown below.'
+
+puts ''
+
+puts 'scp AddToGoogle.safariextz AddToGoogle-update.plist robwilkerson:www/_resources/projects/safari/extensions'
+puts 'scp AddToGoogle-update.plist robwilkerson:www/_resources/projects/safari/extensions/AddToGoogle.plist'
