f41508f1763f72414dd50a700e7e44359140bd1c
Rob Wilkerson <rob@robwilkerson.org>
2010-06-29 15:53:20 -0400

    Correcting a stupid copy/paste error. Either Flickr needs to do away with it's multiple pages sooner rather than later or I need to refactor this to be more DRY. Fixes #6.

--- links.js
+++ links.js
@@ -44,7 +44,7 @@ $(document).ready( function() {
 			: null;
 			
 		return '<ul class="sidecar-list">' +
-					 '<li class="Stats">Image URL: <a href="' + image_uri + '" rel="nofollow">' + truncate( image_uri, 35, true ) + '</a></li>' +
+					 '<li><span class="list-dot"></span>Image URL: <a href="' + image_uri + '" rel="nofollow">' + truncate( image_uri, 35, true ) + '</a></li>' +
 	         '<li><span class="list-dot"></span><a href="http://fiveprime.org/blackmagic" rel="nofollow">View on Black</a></li>' +
 					 '<li><span class="list-dot"></span>View: ' + size_markup() + '</li>' +
 					 '<li><span class="list-dot"></span>Short URL: ' + ( short_uri ? '<a href="' + short_uri + '" rel="nofollow">' + short_uri + '</a>' : 'Not Available' ) + '</li>' +
