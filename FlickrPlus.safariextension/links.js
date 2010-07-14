a339c632ee32c75f2db57ff617ee18855e25c915
Rob Wilkerson <rob@robwilkerson.org>
2010-07-13 20:56:18 -0400

    Requesting and retrieving Exif data via Ajax and returning it to the injected script for insertion into the DOM. I just have to decide how/where to inject it.

--- FlickrPlus.safariextension/links.js
+++ FlickrPlus.safariextension/links.js
@@ -1,76 +1,99 @@
-var new_layout     = document.querySelector( '#shortcuts' ) ? true : false;
-var base_view_uri  = document.querySelector( 'link[rel="canonical"]' ).getAttribute( 'href' ).replace( /\/$/, '' );
-var uri_components = base_view_uri.split( '/' ).slice( -2 );
-var user_id        = uri_components[0];
-var photo_id       = uri_components[1];
-var short_uri      = 'http://flic.kr/p/' + base58( photo_id );
-var img_sizes      = {
-	'Original':  base_view_uri + '/sizes/o',
-	'Large':     base_view_uri + '/sizes/l',
-	'Medium':    base_view_uri + '/sizes/m',
-	'Small':     base_view_uri + '/sizes/s',
-	'Thumbnail': base_view_uri + '/sizes/t',
-	'Square':    base_view_uri + '/sizes/sq'
-}
-var size_markup = function() {
-	var html = new Array();
+/** Only engage if this is a photo page */
+if( document.querySelector( 'meta[name="medium"][content="image"]' ) ) {
+	safari.self.addEventListener( 'message', handle_message, true );
 	
-	for( var size in img_sizes ) {
-		html.push( '<a href="' + img_sizes[size] + '">' + size + '</a>' );
+	var new_layout     = document.querySelector( '#shortcuts' ) ? true : false;
+	var base_view_uri  = document.querySelector( 'link[rel="canonical"]' ).getAttribute( 'href' ).replace( /\/$/, '' );
+	var uri_components = base_view_uri.split( '/' ).slice( -2 );
+	var user_id        = uri_components[0];
+	var photo_id       = uri_components[1];
+	var short_uri      = 'http://flic.kr/p/' + base58( photo_id );
+	var img_sizes      = {
+		'Original':  base_view_uri + '/sizes/o',
+		'Large':     base_view_uri + '/sizes/l',
+		'Medium':    base_view_uri + '/sizes/m',
+		'Small':     base_view_uri + '/sizes/s',
+		'Thumbnail': base_view_uri + '/sizes/t',
+		'Square':    base_view_uri + '/sizes/sq'
 	}
-	
-	return html.join( ', ' );
-}
-
-var base_element = new_layout
-	? document.querySelector( '#sidebar-contexts' )
-	: document.querySelector( '.ContextsOther' );
-
-var flickr_plus = function() {
-	var container = document.createElement( 'div' );
-	var header    = document.createElement( 'h4' );
-	var list      = document.createElement( 'ul' );
-	
-	header.innerHTML = 'Flickr<strong>Plus</strong>';
-	list.appendChild( list_item( 'Image URL: <a href="' + base_view_uri + '" rel="nofollow">' + ( new_layout ? base_view_uri : truncate( base_view_uri, 30, true ) ) + '</a>', true ) );
-	list.appendChild( list_item( '<a href="http://fiveprime.org/blackmagic" rel="nofollow">View on Black</a>' ) );
-	list.appendChild( list_item( 'View: ' + size_markup() ) );
-	list.appendChild( list_item( 'Short URL: ' + ( short_uri ? '<a href="' + short_uri + '" rel="nofollow">' + short_uri + '</a>' : 'Not Available' ), true ) );
-	
-	container.appendChild( header );
-	container.appendChild( list );
-	
-	container.className = new_layout ? 'NeoFlickrPlus' : 'FlickrPlus';
-	
-	return container;
-}
-
-var list_item = function( html, truncate ) {
-	truncate = truncate || false;
-	
-	var item = document.createElement( 'li' );
-	
-	if( new_layout ) {
-		var list_dot = document.createElement( 'span' );
-		list_dot.className = 'list-dot';
+	var size_markup = function() {
+		var html = new Array();
 		
-		item.appendChild( list_dot );
-	}
-	else {
-		item.className = 'Stats';
+		for( var size in img_sizes ) {
+			html.push( '<a href="' + img_sizes[size] + '">' + size + '</a>' );
+		}
+		
+		return html.join( ', ' );
 	}
 	
-	item.innerHTML += html;
-	item.className += truncate ? ' truncate' : '';
+	var base_element = new_layout
+		? document.querySelector( '#sidebar-contexts' )
+		: document.querySelector( '.ContextsOther' );
 	
-	return item;
+	var flickr_plus = function() {
+		var container = document.createElement( 'div' );
+		var header    = document.createElement( 'h4' );
+		var list      = document.createElement( 'ul' );
+		
+		header.innerHTML = 'Flickr<strong>Plus</strong>';
+		list.appendChild( list_item( 'Image URL: <a href="' + base_view_uri + '" rel="nofollow">' + ( new_layout ? base_view_uri : truncate( base_view_uri, 30, true ) ) + '</a>', true ) );
+		list.appendChild( list_item( '<a href="http://fiveprime.org/blackmagic" rel="nofollow">View on Black</a>' ) );
+		list.appendChild( list_item( 'View: ' + size_markup() ) );
+		list.appendChild( list_item( 'Short URL: ' + ( short_uri ? '<a href="' + short_uri + '" rel="nofollow">' + short_uri + '</a>' : 'Not Available' ), true ) );
+		
+		container.appendChild( header );
+		container.appendChild( list );
+		
+		container.className = 'FlickrPlus' + ( new_layout ? ' neo' : '' );
+		
+		return container;
+	}
+	
+	var list_item = function( html, truncate ) {
+		truncate = truncate || false;
+		
+		var item = document.createElement( 'li' );
+		
+		if( new_layout ) {
+			var list_dot = document.createElement( 'span' );
+			list_dot.className = 'list-dot';
+			
+			item.appendChild( list_dot );
+		}
+		else {
+			item.className = 'Stats';
+		}
+		
+		item.innerHTML += html;
+		item.className += truncate ? ' truncate' : '';
+		
+		return item;
+	}
+	
+	base_element.appendChild( flickr_plus() );
+	safari.self.tab.dispatchMessage( 'exif', { 'photo_id': photo_id } );
 }
-
-base_element.appendChild( flickr_plus() );
 
 /**
  * SUPPORTING FUNCTIONS
  */
+function handle_message( msg_event ) {
+	console.log( 'Handling message: ' + msg_event.name );
+	
+	switch( msg_event.name ) {
+		case 'exif':
+			console.log( '  --> exif' );
+			console.log( msg_event.message );
+			break;
+	}
+}
+
+/**
+ * Applies the EXIF data to the bottom of the FlickrPlus element.
+ */
+function flickr_plus_exif( exif ) {
+	var flickr_plus = document.querySelector( '.Flick')
+}
 
 /**
  * Generates the base58 variant of a value. Flickr's short URI ends with
